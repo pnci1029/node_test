86e6eeae2448325fc3fb948acdfa4411804a4748
chhong <pnci1029@gmail.com>
2024-07-19 19:38:10 +0900

    add simple api for test by using node js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const {response, request} = require("express");
 const app = express();
 
 app.listen(8080, function () {
     console.log('123123')
 })
+
+
+const box =
+    `
+    <div>
+        <button>button</button>
+    </div>
+`;
+const responseSomething = (req, res) =>{
+    console.log(req)
+    res.send(box)
+}
+app.get('/',responseSomething)
+
