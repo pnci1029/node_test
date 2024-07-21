0751472aa7d74870689316017fbc5a318e0eba2a
chhong <pnci1029@gmail.com>
2024-07-21 13:34:24 +0900

    add request for testing crawling

--- server.js
+++ server.js
@@ -56,3 +56,36 @@ app.get('/test', function (req, res){
 )
 
 
+/**
+ * 1. 크롤링을 위한 라이브러리 추가
+ * yarn add axios
+ *
+ */
+
+const axios = require('axios');
+
+
+const getHtml = async () => {
+    try {
+        // 1
+        const html = await axios.get("https://www.examples.com");
+        let ulList = [];
+        // 2
+        const $ = cheerio.load(html.data);
+        // 3
+        const bodyList = $("tr.list");
+        bodyList.map((i, element) => {
+            ulList[i] = {
+                rank: i + 1,
+                // 4
+                title: $(element).find("td.info a.title").text().replace(/\s/g, ""),
+                artist: $(element).find("td.info a.artist").text().replace(/\s/g, ""),
+            };
+        });
+        console.log("bodyList : ", ulList);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getHtml().then();
